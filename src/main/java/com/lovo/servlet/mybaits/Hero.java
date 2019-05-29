714183b350d5459a3c41a8b7a46c92263818364d
19960906 <248492911@qq.com>
2019-05-29 21:31:29 +0800

    Initial commit

---
+++ src/main/java/com/lovo/servlet/mybaits/Hero.java
@@ -0,0 +1,35 @@
+package com.lovo.servlet.mybaits;
+
+
+/**
+ * 英雄
+ */
+public class Hero {
+    private String derma;//皮肤
+    private String skill;//技能
+    private String rune;//符文
+
+    public String getDerma() {
+        return derma;
+    }
+
+    public void setDerma(String derma) {
+        this.derma = derma;
+    }
+
+    public String getSkill() {
+        return skill;
+    }
+
+    public void setSkill(String skill) {
+        this.skill = skill;
+    }
+
+    public String getRune() {
+        return rune;
+    }
+
+    public void setRune(String rune) {
+        this.rune = rune;
+    }
+}
---
+++ src/test/java/com/lovo/servlt/mybaits/myTest.java
@@ -0,0 +1,11 @@
+package com.lovo.servlt.mybaits;
+
+
+import org.junit.Test;
+
+public class myTest {
+@Test
+public void test(){
+    System.out.println("helloworld");
+}
+}
